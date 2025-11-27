7fed9c87dabc79af1172a01954eeb2c6f64d1802
jeaninemtn <jeaninemtn@gmail.com>
2025-11-27 14:07:22 +0800

    update module

--- 16.module.ts
+++ 16.module.ts
@@ -1,9 +1,12 @@
 // module 模組
 
+// https://www.typescriptlang.org/docs/handbook/2/modules.html
+
+// 語法
+
 // 1. ES6 module syntax (ESM)
 // import
 // export
-
 
 // 2. CommonJS(CJS)
 // require
@@ -11,6 +14,7 @@
 
 
 // ---
+
 
 // ES6
 
@@ -37,5 +41,26 @@ A top-level 'export' modifier cannot be used on value declarations in a CommonJS
 // TS不會再幫忙修改、自動修正import/export
 // 也就是TS不會把ESM轉成CJS的模組語法
 
+
+// ---
+
+
 // CommonJS
 // module.exports = { ... }
+
+// import func = require("func");
+// const code = func.readFileSync("module.ts", "utf8");
+
+
+// ---
+
+
+export type Cat = { name: string, age: number };
+export interface Dog {
+    name: string;
+    age: number;
+};
+
+// 在其他檔案中引入時 使用import
+// import { Cat, Dog } from "./16.module";
+type Animals = Cat | Dog;
